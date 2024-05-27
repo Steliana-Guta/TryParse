0f7bb73258bfef42f4fc0fb6857b082d404061f5
Steliana-Guta <steliana.m.guta@gmail.com>
2024-05-27 18:42:36 +0100

    Add project files.

---
+++ TryParse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age in full years :");
+            string inputAgeString =  Console.ReadLine();
+            int inputAge = Convert.ToInt32(inputAgeString);
+            Int32.TryParse(inputAgeString, out inputAge);
+            if (inputAge > 0)
+            {
+                Console.WriteLine("You are " + inputAge + " years old.");
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid answear");
+           //to accept 21.5 i would change the parse from int32 to decimal
+            }
+        }
+    }
+}
